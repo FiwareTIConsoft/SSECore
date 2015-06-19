3c887f028101bbac89fff8c3c9124d8f6d433dd8
Andrea Antonazzo <andrea.antonazzo@guest.telecomitalia.it>
2015-06-19 11:46:37 +0200

    Added Grizzly shutdown hook

--- SSECoreServer/src/main/java/com/tilab/ca/sse/core/ssecoreserver/App.java
+++ SSECoreServer/src/main/java/com/tilab/ca/sse/core/ssecoreserver/App.java
@@ -4,10 +4,11 @@ import com.tilab.ca.sse.core.lucene.IndexesUtil;
 import java.io.IOException;
 import java.net.URI;
 import java.util.Date;
-import java.util.logging.Level;
+import java.util.concurrent.TimeUnit;
 import org.apache.log4j.Logger;
 import org.aeonbits.owner.ConfigCache;
 import org.aeonbits.owner.event.ReloadEvent;
+import org.glassfish.grizzly.GrizzlyFuture;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.accesslog.AccessLogBuilder;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
@@ -40,22 +41,26 @@ public class App {
 
         // to enable Multipart support
         rc.register(MultiPartFeature.class);
-		
 
-		
-		
         IndexesUtil.init();
         sseConfigFromCache = ConfigCache.getOrCreate(SSEConfig.class);
         sseConfigFromCache.addReloadListener((ReloadEvent event) -> {
-            LOG.info("Reload intercepted at "+ new Date());
+            LOG.info("Reload intercepted at " + new Date());
         });
 
         HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(URI.create(sseConfigFromCache.serviceUrl()), rc, false, null, false);
         enableAccessLog(httpServer);
+
+        // register shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            LOG.info("shutdown hook invoked, stopping Grizzly server");
+            shutdownGrizzlyWebServer(httpServer);
+        }, "shutdownHook"));
+
         try {
             httpServer.start();
         } catch (Exception e) {
-            LOG.error( "could not start Grizzly server", e);
+            LOG.error("could not start Grizzly server", e);
             throw new RuntimeException(e);
         }
 
@@ -75,7 +80,7 @@ public class App {
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
 
-        LOG.info("Jersey app started with WADL available at "+ sseConfigFromCache.serviceUrl());
+        LOG.info("Jersey app started with WADL available at " + sseConfigFromCache.serviceUrl());
         Thread.currentThread().suspend(); //XXX verify the best option to suspend the current thread
 
         server.stop();
@@ -112,6 +117,26 @@ public class App {
          sc.getMonitoringConfig().getWebServerConfig().addProbes(alp);*/
         final AccessLogBuilder builder = new AccessLogBuilder("./sse_core_access.log");
         builder.instrument(httpServer.getServerConfiguration());
+
+    }
+
+    protected static void shutdownGrizzlyWebServer(HttpServer grizzlyWebServer) {
+        if (grizzlyWebServer != null && grizzlyWebServer.isStarted()) {
+            // Gracefully stops the transport accepting new connections and allows existing work to complete before 
+            // finalizing the shutdown. This method will wait for the specified time for all interested parties to signal 
+            // it is safe to terminate the transport.
+            // If the timeout is exceeded, the transport will be terminated forcefully.
+            GrizzlyFuture<HttpServer> future = grizzlyWebServer.shutdown(10000, TimeUnit.MILLISECONDS);
+            //Returns true if this task completed
+            while (!future.isDone()) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ignore) {
+                    LOG.error("ignore error = ", ignore);
+                }
+            }
+
+        }
 
     }
 
