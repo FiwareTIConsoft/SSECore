308bb22498cc3adbe9d259c7f6306c748ae68d3d
Riccardo <riccardo.muzzi@guest.telecomitalia.it>
2015-06-29 16:51:47 +0200

    RPM update script fix - bad interpreter

--- SSECoreServer/src/main/bin/update_ssecore.sh
+++ SSECoreServer/src/main/bin/update_ssecore.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 curl "http://ca.tilab.com/builds/SSEcore/last/ssecore-install-package.noarch.rpm" > "/tmp/ssecore-install-package.noarch.rpm"
 rpm -ev ssecore-install-package.noarch
 rpm -ivh /tmp/ssecore-install-package.noarch.rpm
